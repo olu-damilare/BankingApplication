cbbe6728c3b3345d1748124db3793df714302aa8
Damilare Jolayemi <djolayemi@gmail.com>
2021-04-11 14:52:52 +0100

    Update

---
+++ src/africa/semicolon/bankingApplication/Address.java
@@ -0,0 +1,11 @@
+package africa.semicolon.bankingApplication;
+
+public class Address {
+    private final String houseNumber;
+    private final String streetName;
+
+    public Address(String houseNumber, String streetName) {
+        this.houseNumber = houseNumber;
+        this.streetName = streetName;
+    }
+}
