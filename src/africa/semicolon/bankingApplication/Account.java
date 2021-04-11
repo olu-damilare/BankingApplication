80084a76bb73868eb9122e2f7456fe54f1fa4353
Damilare Jolayemi <djolayemi@gmail.com>
2021-04-11 23:40:51 +0100

    Update

--- src/africa/semicolon/bankingApplication/Account.java
+++ src/africa/semicolon/bankingApplication/Account.java
@@ -1,17 +1,27 @@
 package africa.semicolon.bankingApplication;
 
 public class Account {
-    private final Object customerID;
+    private final int customerID;
     private final String accountNumber;
-    private AccountType accountType;
+    private final String accountName;
+    private final AccountType accountType;
 
     public Account(Customer customer, AccountType accountType, String accountNumber) {
         this.accountType = accountType;
-        this.customerID = customer.getCustomerID();
+        customerID = customer.getCustomerID();
         this.accountNumber = accountNumber;
+        accountName = customer.getFullName();
     }
 
     public AccountType getAccountType() {
         return accountType;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountName() {
+        return accountName;
     }
 }
