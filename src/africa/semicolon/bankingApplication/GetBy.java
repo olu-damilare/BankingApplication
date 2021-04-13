42ae877fbcc12e9ae4c3bc06ba40fa235a2f8850
Damilare Jolayemi <djolayemi@gmail.com>
2021-04-13 04:12:56 +0100

    Update

--- src/africa/semicolon/bankingApplication/GetBy.java
+++ src/africa/semicolon/bankingApplication/GetBy.java
@@ -1,5 +1,17 @@
 package africa.semicolon.bankingApplication;
 
+import java.util.ArrayList;
+
 public interface GetBy {
     Customer getCustomer(String accountNumber);
+
+    Customer getCustomer(int customerID);
+
+    String getAccountNumber(Customer customer, AccountType current);
+
+    Account getAccount(Customer customer, AccountType accountType);
+
+    Account getAccount(String accountNumber);
+
+    ArrayList<Account> getAccounts(int customerID);
 }
