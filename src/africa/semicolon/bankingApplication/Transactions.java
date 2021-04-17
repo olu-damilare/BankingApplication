cfe276fb304d1a29d80fda3bac8f35d072ebbb49
Damilare Jolayemi <djolayemi@gmail.com>
2021-04-17 23:18:34 +0100

    Update

--- src/africa/semicolon/bankingApplication/Transactions.java
+++ src/africa/semicolon/bankingApplication/Transactions.java
@@ -7,7 +7,13 @@ public interface Transactions {
     void deposit(String accountNumber, BigDecimal amount);
 
     BigDecimal getBalance(String accountNumber);
-    public void withdraw(String accountNumber, BigDecimal amount, String pin);
+    void withdraw(String accountNumber, BigDecimal amount, String pin);
 
     void transfer(String senderAccountNumber, String beneficiaryAccountNumber, BigDecimal amount , String senderAccountPin);
+
+    void updateFirstName(Customer customer, String newFirstName);
+
+    void updateMiddleName(Customer customer, String newMiddleName);
+
+    void updatePhoneNumber(Customer customer, String newPhoneNumber);
 }
